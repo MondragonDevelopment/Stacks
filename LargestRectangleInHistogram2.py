14c336fc9b204e4348811b25e7bdcb59c2706675
Mondragon <fimv705@gmail.com>
2024-05-19 02:11:06 -0600

    Read the comments, nice tech

--- LargestRectangleInHistogram2.py
+++ LargestRectangleInHistogram2.py
@@ -5,15 +5,15 @@ Given an array of integers heights representing the histogram's bar height where
 def largestRectangleArea(heights):
     maxArea = 0
     stack = []
-    for i, h  in enumerate(heights):
+    for i, h  in enumerate(heights):                    # Next time I need to keep track of both index and value I must use enumerate
         start = i
         while stack and stack[-1][1] > h:
-            index, height = stack.pop()
+            index, height = stack.pop()                 # I have to remember this shortcut (popping while storing the value)    
             maxArea = max(maxArea, height*(i - index))
-            start = index
-        stack.append((start, h))
+            start = index                               # With this line you take into account the previous columns that sum for the total base of the new rectangle
+        stack.append((start, h))                        # Here you storage the info form the previous comment
     for i, h in stack:
-        maxArea = max(maxArea, h*(len(heights) - i))
+        maxArea = max(maxArea, h*(len(heights) - i))    # Here lie every rectangle that could be made from the beginning of the list
     return maxArea
             
 
